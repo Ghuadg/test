29d21035382b85659d8e822a8a7cef15e03d76d0
Ghuadg <maxim14790147@gmail.com>
2024-07-02 23:18:44 +0500

    test

---
+++ internetshop/shop/admin.py
@@ -0,0 +1,11 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
